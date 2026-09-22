d6ef3875f5611975e26624fa70fa5546e1cb9f9f
meharinkoommen <meharinkoommen@gmail.com>
2026-09-22 17:48:31 +0530

    Add files via upload

---
+++ p1.py
@@ -0,0 +1,61 @@
+
+
+'''
+
+1. studentname
+2.studentbranch
+3.studentage
+4.studentgender
+
+
+studentname 
+fathername
+address
+pincode
+mobilenumber
+intermarks
+islogin
+
+'''
+
+student_name="vaishnavi"
+student_branch="cse"
+student_age=20
+student_gender="female"
+
+print(student_name)
+print("student name is: ",student_name)
+print(student_age)
+print("student age is",student_age)
+print(student_branch)
+print("student branch is",student_branch)
+print(student_gender)
+print("student gender is",student_gender)
+
+#student name is vaishnavi
+#student branch is cse
+
+student_name="Meharin"
+father_name="Oommen"
+address="Kerala"
+pincode=678901
+mobile_number=7416869546
+inter_marks=98
+is_login=True
+
+print("Student Name:", student_name)
+print("Father Name:", father_name)
+print("Address:", address)
+print("Pin Code:", pincode)
+print("Mobile Number:", mobile_number)
+print("Intermediate Marks:", inter_marks)
+print("Is Login:", is_login)
+
+#f string or string literal
+
+a=10
+b=20
+print(a+b)
+# addition of 10 and 20 is 30
+print("Addition of", a, "and", b, "is", a+b)
+print(f"Addition of {a} and {b} is {a+b}")
